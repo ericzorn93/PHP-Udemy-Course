686b199327b213fc3a146a859bea4f95a890b9f9
Eric Zorn <goldencatch93@gmail.com>
2018-01-30 17:36:27 -0500

    Using the GET Superglobal Finished

--- demo-2/the_post.php
+++ demo-2/the_post.php
@@ -1,9 +1,3 @@
-<?php
-
-    print_r($_GET);
-
-?>
-
 <!doctype html>
 <html lang="en">
 <head>
@@ -11,18 +5,9 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>GET Superglobal PHP</title>
+    <title>Document</title>
 </head>
 <body>
-
-<?php
-
-    $id = 10;
-    $button = "CLICK HERE NOW";
-
-?>
-
-<a href="get.php?id=<?php echo $id; ?>"><?php echo $button; ?></a>
 
 </body>
 </html>
