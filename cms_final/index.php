6e1e1cdcaa135e02dd5f7fd0a774b0a03cb26c10
Eric Zorn <goldencatch93@gmail.com>
2018-02-08 17:53:26 -0500

    Adding Dynamic Images from Database

--- cms_final/index.php
+++ cms_final/index.php
@@ -45,7 +45,7 @@
                 </p>
                 <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date; ?></p>
                 <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="<?php echo $post_image; ?>">
                 <hr>
                 <p><?php echo $post_content; ?></p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
