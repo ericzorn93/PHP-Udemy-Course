3a306fc60e278db05f2a47a40d15001fa56a6d70
Eric Zorn <goldencatch93@gmail.com>
2018-02-16 17:24:18 -0500

    Editing Navigation and Categories

--- cms_final/admin/categories.php
+++ cms_final/admin/categories.php
@@ -17,7 +17,6 @@
             <div class="row">
                 <div class="col-lg-12">
 
-
                     <h1 class="page-header">
                         Welcome to Admin
                         <small>Author</small>
@@ -33,6 +32,23 @@
                                 <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
                             </div>
                         </form>
+                    </div> <!-- Add Category Form -->
+
+                    <div class="col-xs-6">
+                        <table class="table table-bordered table-hover">
+                            <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Category Title</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>Baseball Category</td>
+                                <td>Basketball Category</td>
+                            </tr>
+                            </tbody>
+                        </table>
                     </div>
 
 
