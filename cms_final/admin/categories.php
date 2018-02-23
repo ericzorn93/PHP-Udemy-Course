acb3f426d2ac10d6e6f3a51fd8518d52ea786658
Eric Zorn <goldencatch93@gmail.com>
2018-02-22 19:08:54 -0500

    Delete Category Query and Reroute

--- cms_final/admin/categories.php
+++ cms_final/admin/categories.php
@@ -59,10 +59,6 @@
                     </div> <!-- Add Category Form -->
 
                     <div class="col-xs-6">
-                    <?php
-                        $query = "SELECT * FROM categories";
-                        $select_categories = mysqli_query($connection, $query);
-                    ?>
                         <table class="table table-bordered table-hover">
                             <thead>
                             <tr>
@@ -73,6 +69,10 @@
                             <tbody>
                             <tr>
                                 <?php
+                                // FIND ALL CATEGORIES QUERY
+                                $query = "SELECT * FROM categories";
+                                $select_categories = mysqli_query($connection, $query);
+
                                 while ($row = mysqli_fetch_assoc($select_categories)) {
                                     $cat_id = $row['cat_id'];
                                     $cat_title = $row['cat_title'];
@@ -81,10 +81,25 @@
                                     echo"<tr>";
                                         echo "<td>{$cat_id}</td>";
                                         echo "<td>{$cat_title}</td>";
+                                        echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
                                     echo "</tr>";
                                 }
                                 ?>
                             </tr>
+
+                            <?php
+
+                            if (isset($_GET['delete'])) {
+                                // Delete Category Query and Reroute
+                                $the_cat_id = $_GET['delete'];
+                                $query = "DELETE FROM categories WHERE cat_id = {$the_cat_id} ";
+                                $delete_query = mysqli_query($connection, $query);
+
+                                // Reload Page
+                                header("Location: categories.php");
+                            }
+
+                            ?>
                             </tbody>
                         </table>
                     </div>
--- cms_final/admin/includes/admin_navigation.php
+++ cms_final/admin/includes/admin_navigation.php
@@ -47,11 +47,11 @@
                     </li>
                 </ul>
             </li>
-            <li>
-                <a href="bootstrap-grid.html"><i class="fa fa-fw fa-wrench"></i> Categories</a>
-            </li>
             <li class="active">
-                <a href="categories.php"><i class="fa fa-fw fa-file"></i> Comments</a>
+                <a href="categories.php"><i class="fa fa-fw fa-wrench"></i> Categories</a>
+            </li>
+            <li class="">
+                <a href="#"><i class="fa fa-fw fa-file"></i> Comments</a>
             </li>
             <li>
                 <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Users <i class="fa fa-fw fa-caret-down"></i></a>
